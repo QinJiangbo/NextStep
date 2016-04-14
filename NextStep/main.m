d8dff88b9ff3b30b6ca00777366c16dfe915c2b7
QinJiangbo <QinJiangbo@users.noreply.github.com>
2016-04-14 20:02:27 +0800

    Initial Commit

---
+++ NextStep/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NextStep
+//
+//  Created by Richard on 4/14/16.
+//  Copyright © 2016 Richard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
